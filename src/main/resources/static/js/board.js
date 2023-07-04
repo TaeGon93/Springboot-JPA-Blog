c91a3d275093222a9275636646c6dd35b8821bea
TaeGon Kim <eyeyo93@gmail.com>
2023-07-04 23:32:30 +0900

    20230704

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -4,6 +4,12 @@ let index = {
 		$("#btn-save").on("click", () => {//function(){}, ()=>{} this를 바인딩하기 이해서
 			this.save();
 		});
+		$("#btn-delete").on("click", () => {//function(){}, ()=>{} this를 바인딩하기 이해서
+			this.deleteById();
+		});
+		$("#btn-update").on("click", () => {//function(){}, ()=>{} this를 바인딩하기 이해서
+			this.update();
+		});
 	},
 
 	save: function() {
@@ -17,16 +23,53 @@ let index = {
 			//회원가입 수행 요청
 			type: "POST",
 			url: "/api/board",
-			data: JSON.stringify(data), 
+			data: JSON.stringify(data),
 			contentType: "application/json; charset=utf-8",
-			dataType: "json" 
+			dataType: "json"
 		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
+	},
 
+	deleteById: function() {
+		let id = $("#id").text();
+
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/" + id,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+	
+		update: function() {
+		let id = $("#id").val();
+			
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val(),
+		};
+
+		$.ajax({
+			//회원가입 수행 요청
+			type: "PUT",
+			url: "/api/board/"+id,
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("수정이 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
 	},
 }
 
